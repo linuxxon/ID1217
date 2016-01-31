f1a17621e2e0dd7c6c284f76bc925a2219c8706d
Rasmus Linusson <raslin@kth.se>
2016-01-31 22:49:22 +0100

    README and test script

--- hw1/tee/run.sh
+++ hw1/tee/run.sh
@@ -2,41 +2,48 @@
 #
 # Homework 1 - Problem 4: UNIX tee
 # 
+# Script will run several tests to weed out some suspicions of race conditions
+#
+# The basis for the test is described in README
 #
 # As per requirements paragraph 2, section 4 a script to run the
 # homework must be included.
 
-# Make tee
-make
+# Make tee, no ugly prints
+make 2>&1 > /dev/null
 
-# Run test
-./tee test.outfile < text > test.stdout
+# Number of times to run the test
+iterations=100
 
-# Variable to save result: 0 - Passed, 1 - Failed
-result=1
+echo "Running test for $iterations iterations..."
 
-# Determine the result
-cmp test.outfile text
-if [ $? -eq 0 ]; then
-    cmp test.stdout text
+for i in $(seq $iterations); do
+    # Run test
+    ./tee "test.outfile" < text > "test.stdout"
+
+    # Determine the result
+    cmp test.outfile text
     if [ $? -eq 0 ]; then
-        result=0
+        cmp test.stdout text
+        if [ $? -eq 0 ]; then
+            result=0
+        else
+            result=1
+        fi
     else
         result=1
     fi
-else
-    result=1
-fi
 
-# Clean up the output files
-rm -f test.outfile
-rm -f test.stdout
+    # Clean up the output files
+    rm -f test.outfile
+    rm -f test.stdout
 
-# Print and return result
-if [ $result -eq 0 ]; then
-    echo "Test passed successfully"
-    exit 0
-else
-    echo "Test failed!"
-    exit 1
-fi
+    # Print and return result
+    if [ $result -ne 0 ]; then
+        echo "Test failed!"
+        exit 1
+    fi
+done
+
+# If script got here everything is great!
+echo "Test passed successfully!"
