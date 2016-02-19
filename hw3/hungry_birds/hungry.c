ffa6692d80fd7916f8460a89a94619da16ee1563
Rasmus Linusson <rasmus@linusson.org>
2016-02-19 18:09:53 +0100

    Comments for hw3p1

--- hw3/hungry_birds/hungry.c
+++ hw3/hungry_birds/hungry.c
@@ -2,6 +2,16 @@
  *
  * Homework 3 - Problem 1: The Hungry Birds Problem (20 points)
  *
+ * The question of fairness (Same as in README):
+ *  A part of the assignment is to make a comment as to the fairness of the
+ *  algorithm. This implementation is **NOT**  fair with regard to the birds,
+ *  this wholly because of the implementation of the semaphores. 
+ *  The semaphores are kind of similar to first come, first serve and holds no
+ *  regard as to the invocation order of 'sem_wait'. 
+ *  With this behavior there is no guarantee that every baby bird will get to
+ *  eat as there is a risk of the other baby birds always getting through the
+ *  semaphore every time there are worms available.
+ *
  * Semantics:
  *  See full description in README
  *
@@ -14,7 +24,7 @@
  * For course ID1217 at KTH
  *
  * Author: Rasmus Linusson
- * Last modified: 17/02-2016
+ * Last modified: 19/02-2016
  */ 
 
 #ifndef _REENTRANT
@@ -44,27 +54,41 @@
 /* Set done to non-zero to kill threads */
 int done = 0;
 
+/* Simulation settings */
 int worm_addition;
 int dish_capacity;
 
+/* Semaphores */
 sem_t check_dish;
 sem_t get_worm;
 sem_t go_fetch;
 
+/* Thread functions */
 void* baby_bird(void* arg);
 void* parent_bird(void* arg);
 
+/* Entry point
+ *
+ * Takes user arguments, inits variable and spawn threads
+ *
+ * Takes three optional arguments:
+ *  First: int - number of baby birds in the nest
+ *  Second: int - maximum number of worms kept in the dish
+ *  Third: int - maximum number of worms parent bird might find
+ */
 int main(int argc, char* argv[])
 {
     long i;
     char x[2];
+
+    /* Set defaults */
     int num_birds = DEFAULT_BIRDS;
     dish_capacity = DEFAULT_CAPACITY;
     worm_addition = DEFAULT_WORM_ADDITION;
 
-    pthread_t tid;
+    pthread_t tid;          /* Throwaway id */
 
-    /* Get invoaktion arguments */
+    /* Get arguments */
     if (argc > 1)
         num_birds = atoi(argv[1]) > MAX_BIRDS ? MAX_BIRDS : atoi(argv[1]);
     if (argc > 2)
@@ -74,18 +98,20 @@ int main(int argc, char* argv[])
 
     /* Init semaphores */
     sem_init(&get_worm, 0, 0);          /* Empty dish */
-    sem_init(&check_dish, 0, 1);
-    sem_init(&go_fetch, 0, 0);    /* Noone has complained yet */
+    sem_init(&check_dish, 0, 1);        /* Mutex */
+    sem_init(&go_fetch, 0, 0);          /* Noone has complained yet */
 
-    printf("Welcome to The Hungry Birds Problem!\nThere are %d baby birds and 1 \
-            parent bird\nThe dish can hold %d worms and the parent fetches 1 - %d \
-            new worms every time it is woken.\nLet's start!\n",
+    /* Print welcome message */
+    printf("Welcome to The Hungry Birds Problem!\nThere are %d baby birds and 1"
+            "parent bird\nThe dish can hold %d worms and the parent fetches 1 - %d"
+            "new worms every time it is woken.\nLet's start!\n",
             num_birds, dish_capacity, worm_addition);
 
     /* Spawn children */
     for (i = 0; i < num_birds; i++)
         pthread_create(&tid, NULL, baby_bird, (void*)i);
 
+    /* Spawn parent */
     pthread_create(&tid, NULL, parent_bird, NULL);
     
     /* Main thread is waiting for anything to exit */
@@ -100,11 +126,18 @@ int main(int argc, char* argv[])
     pthread_exit(0);
 }
 
+/* baby_bird - function describing behaviour of baby birds
+ *
+ * Arguments
+ *  void* arg - long baby bird id cast to a void pointer
+ */
 void* baby_bird(void* arg)
 {
     pthread_detach(pthread_self());
 
     int id = (int)(long)arg;
+
+    /* Local variables to keep result from sem_getvalue */
     int num_worms;
     int told_parent;
 
@@ -115,11 +148,11 @@ void* baby_bird(void* arg)
 
         sem_getvalue(&get_worm, &num_worms);
 
-        if (num_worms > 0) {
+        if (num_worms > 0) {        /* Worms available - eat them */
             sem_wait(&get_worm);    /* Garanteed to work as locked by check_dish */
             sem_post(&check_dish);  /* Release lock */
         }
-        else {
+        else {                      /* No worms available */
             /* Only one baby bird should chirp loudly */
             sem_getvalue(&go_fetch, &told_parent);
             if (!told_parent) {
@@ -133,7 +166,7 @@ void* baby_bird(void* arg)
 
         printf("Baby bird %d just ate\n", id);
 
-        /* Sleep random time using utime */
+        /* Sleep random time */
         usleep(rand() % MAX_SLEEP + MIN_SLEEP + 1);
     }
 
@@ -142,6 +175,7 @@ void* baby_bird(void* arg)
     pthread_exit(0);
 }
 
+/* parent_bird - function defining behavior of parent bird */
 void* parent_bird(void* arg)
 {
     pthread_detach(pthread_self());
@@ -160,7 +194,6 @@ void* parent_bird(void* arg)
 
         /* Fly off */
         /* Random time delay? */
-        /* Nah, orka */
 
         /* Add worms to dish */
         worms_fetched = rand() % worm_addition + 1;
